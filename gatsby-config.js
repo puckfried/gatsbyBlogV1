5c4a97f805d5de03a0012f8e7fe12057170166d8
puckfried <puckfried@posteo.de>
2021-12-21 13:36:55 +0100

    newTitle

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "a title??",
+    title: "a new title???",
   },
   plugins: [{
     resolve: "gatsby-source-filesystem",
